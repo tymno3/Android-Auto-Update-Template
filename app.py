09ccc1272fe030a241f0146ab6c48f95e1a37d7e
Timothy Ding <tding215@gmail.com>
2020-06-26 19:42:37 -0700

    Working
    Made it so that it will print the greatest possible version

--- app.py
+++ app.py
@@ -6,10 +6,28 @@ app = Flask(__name__)
 db = TinyDB('db.json')
 
 
-@app.route('/', methods=['GET'])
+@app.route("/")
+def index():
+    return "Homepage"
+
+
+@app.route('/version/', methods=['GET'])
 def get_app_versions():
-    print(f'Request: {request}')
-    return json.dumps({"items": db.all()})
+    # print(f'Request: {request}')
+    username = request.args.get('username')
+    current_version = request.args.get('current_version')
+    desired_version = get_desired_version(username, current_version)
+    return json.dumps({"desired_version": desired_version})
+
+
+def get_desired_version(username, current_version):
+    # todo(tding): Actually use username and current version to determine the highest version
+    return highest_version()
+
+
+def highest_version():
+    all_versions = [row['version'] for row in db.all()]
+    return max(all_versions)
 
 
 if __name__ == '__main__':
